24eb76562ec2dd35f6da5a636c1fef4c0aa3ad83
Ayush James <ayush.james1996@gmail.com>
2019-11-01 15:04:51 +1100

    temp fix for if user makes mistake in spelling a planet

--- exercises/day3SpaceAge/spaceage.js
+++ exercises/day3SpaceAge/spaceage.js
@@ -9,6 +9,8 @@ const planetData = {
   neptune: 164.79132
 }
 
+const milkyway = ["earth", "mercury", "venus", "mars", "jupiter", "saturn", "uranus", "neptune"];
+
 function age(seconds, planet) {
   // This will allow the users to input it as Uppercase, Lowercase or Capital and this function should still work the same!
   let planetLowerCase = planet.toLowerCase();
@@ -17,16 +19,20 @@ function age(seconds, planet) {
 
   // want to see the age according to all planets.
   if (planetLowerCase === "all" ) {
+    console.log(`Input: "${planet}"`);
     for (let planetBody in planetData) {
       let age = (earthAge / planetData[planetBody]).toFixed(2);
-      console.log(`You are ${age} ${planetBody.charAt(0).toUpperCase() + planetBody.slice(1)}-years old`);
+      console.log(`You are ${age} ${planetBody.charAt(0).toUpperCase() + planetBody.slice(1)}-years old.`);
       // This is taking the first character of the string changing into to uppercase and them combining it with the slice of the rest of the string - So as to capitalise the first names.
     }
 
   //seeing age to a specific planet!
-  } else {
+  } else if (milkyway.includes(planetLowerCase)) {
     let age = (earthAge / planetData[planetLowerCase]).toFixed(2);
-    console.log(`You are ${age} ${planetLowerCase.charAt(0).toUpperCase() + planetLowerCase.slice(1)}-years old`);
+    console.log(`Input: "${planet}" - You are ${age} ${planetLowerCase.charAt(0).toUpperCase() + planetLowerCase.slice(1)}-years old.`);
+
+  } else {
+    console.log(`Input: "${planet}" - Please, Try Again! That Planet is not part of the Milky Way Galaxy.`)
   }
 }
 
@@ -34,5 +40,6 @@ age(1000000000, "Mercury");
 age(1000000000, "MERCURY");
 age(1000000000, "mercury");
 age(1000000000, "all");
+age(1000000000, "fsfds");
 
-//Need to figure out how to catch for errors in spelling of plants! currenly would come as NaN
+//Need to figure out a better fix for how to catch for errors in spelling of plants! currenly would come as NaN
